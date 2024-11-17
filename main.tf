eb52700008d0901b04428e8bb4104c6227c5ff06
adieos <athanokwibisono@gmail.com>
2024-11-17 17:26:33 +0700

    feat: ip gw subnet A1-A4

--- main.tf
+++ main.tf
@@ -14,6 +14,65 @@ provider "proxmox" {
   insecure = true
 }
 
+// node DONE
+resource "proxmox_virtual_environment_vm" "nasi-uduk-D06" {
+    name = "nasi-uduk-D06"
+    node_name = "its"
+    on_boot = true
+    stop_on_destroy = true
+    scsi_hardware = "virtio-scsi-single"
+    vm_id = lookup(var.vm_id_list, "nasi-uduk-D06")
+
+    clone {
+      datastore_id = "local-lvm"
+      node_name = "its"
+      vm_id = 5555
+    }
+    
+    agent {
+      enabled = false
+    }
+
+    initialization {
+      ip_config { #eth0
+        ipv4 {
+          address = lookup(var.ip_list, "nasi-uduk-D06")
+          gateway = lookup(var.gateaway_list, "nasi-uduk-D06")
+        }
+      }
+      user_account {
+        username = "D06"
+        password = "hmd"
+      }
+    }
+
+    cpu {
+      cores = 1
+      type = "x86-64-v2-AES"
+    }
+
+    memory {
+      dedicated = 1024 # 1GB
+      floating = 1024
+    }
+
+    disk {
+      datastore_id = "local-lvm"
+      file_id = "local:iso/focal-server-cloudimg-amd64.img"
+      file_format = "raw"
+      interface = "virtio0"
+      iothread = true
+      size = 3 # GB
+    }
+
+    network_device {
+      enabled = true
+      firewall = false
+      bridge = "vmbr0"
+    }
+}
+
+// node WIP
 resource "proxmox_virtual_environment_vm" "nasi-uduk-D06" {
     name = "nasi-uduk-D06"
     node_name = "its"
--- variables.tf
+++ variables.tf
@@ -18,13 +18,42 @@ variable "vm_id_list" {
 variable "ip_list" {
   type        = map(string)
   default = {
+    // A1
     "nasi-uduk-D06" = "192.168.6.34/29"
+    "geprek-D06" = "192.168.6.35/29"
+    "alfamart-sw2" = "192.168.6.33/29"
+
+    // A2
+    "kwetiaw-D06" = "192.168.6.2/30"
+    "alfamart-sw1" = "192.168.6.1/30"
+
+    // A3
+    "alfamart-eth0" = "192.168.6.6/30"
+    "infomaret-eth1" = "192.168.6.5/30"
+
+    // A4
+    "indomaret-eth0" = "192.168.6.10/30"
+    "family-mart-eth1" = "192.168.6.9/30"
+
+    // A5
+    
   }
 }
 
 variable "gateaway_list" {
   type        = map(string)
   default = {
+    // A1
     "nasi-uduk-D06" = "192.168.6.33"
+    "geprek-D06" = "192.168.6.33"
+
+    // A2
+    "kwetiaw-D06" = "192.168.0.1"
+
+    // A3
+    "alfamart-eth0" = "192.168.6.5"
+
+    // A4
+    "indomaret-eth0" = "192.168.6.9"
   }
 }
